cdc32fd964cca500cf10ff9c952861b9edf404f4
maiaboelmagde <102434408+maiaboelmagde@users.noreply.github.com>
2025-04-26 22:26:39 +0300

    set routes, design LoginPage & HomePage & create some components

--- 12-Angular Fundamentals/Day5/Lab5/src/app/app.routes.ts
+++ 12-Angular Fundamentals/Day5/Lab5/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
+import { ProductComponent } from './components/product/product.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'home',component:MainComponent},
+    {path:'product',component:ProductComponent},
+    {path:'**',component:NotFoundComponent}
+];
---
+++ 12-Angular Fundamentals/Day5/Lab5/src/app/components/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Page2Component } from '../page2/page2.component';
+
+@Component({
+  selector: 'app-main',
+  imports: [Page2Component],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
