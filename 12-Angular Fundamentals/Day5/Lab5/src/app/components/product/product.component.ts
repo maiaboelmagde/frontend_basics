f637652bae53c8134ef11a7954da64f317406422
maiaboelmagde <102434408+maiaboelmagde@users.noreply.github.com>
2025-04-26 23:03:08 +0300

    adding product page conrent & registration UI & notFound component UI

--- 12-Angular Fundamentals/Day5/Lab5/src/app/components/product/product.component.ts
+++ 12-Angular Fundamentals/Day5/Lab5/src/app/components/product/product.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [HomeComponent,FormComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
